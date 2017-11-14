6c7d25ed24a57ec9b846a7f833e6a79c31b82986
Frederic MEYER <fmeyer@nearbysoftware.com>
2017-11-14 11:06:30 +0100

    Removed duplicate assembly metadata

--- src/Demo/Demo/Properties/AssemblyInfo.cs
+++ src/Demo/Demo/Properties/AssemblyInfo.cs
@@ -8,7 +8,6 @@ using Xamarin.Forms.Internals;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyDescription("")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
